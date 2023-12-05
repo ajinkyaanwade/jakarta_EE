4dac96f3a89beae578719e9219ab48cf9fcc190f
ajinkyaanwade <147714454+ajinkyaanwade@users.noreply.github.com>
2023-12-05 08:33:54 +0530

    Add files via upload
    advance java

---
+++ First/src/main/java/IncluderServ.java
@@ -0,0 +1,40 @@
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class IncluderServ
+ */
+public class IncluderServ extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		try
+		{
+
+			PrintWriter pw=response.getWriter();
+			pw.println("<b>this is a main servlet</b><br>");
+			RequestDispatcher rd=request.getRequestDispatcher("IncludedServ");
+			rd.include(request,response);
+			pw.println("<b>coming back to main servlet</b>");
+			
+		}
+		catch(Exception ee)
+		{
+			System.out.println(ee);
+		}
+	}
+
+}
---
+++ First_init_method/src/main/java/First.java
@@ -0,0 +1,48 @@
+
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class First
+ */
+public class First extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		try
+		{
+		PrintWriter pw=response.getWriter();
+		pw.println("Overriding init(ServletConfig conf)");
+		/* NullPointerException for following 2 statements*/
+		pw.println("Config parameter value  "+config.getInitParameter("user"));
+		pw.println("Context parameter value   "+getServletContext().getInitParameter("database"));
+		}
+		catch(Exception ee)
+		{
+			ee.printStackTrace();
+		}
+	}
+	private ServletConfig config;
+	@Override
+	public void init(ServletConfig config) throws ServletException
+	{
+		// TODO Auto-generated method stub
+		this.config=config;
+	}
+
+	
+
+}
