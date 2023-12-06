764a3d49f9cd22b67ab52fa7dd18c585a688b691
ajinkyaanwade <ajinkya.anwade512000@gmail.com>
2023-12-06 23:50:42 +0530

    1

--- Day1.java
+++ Day1.java
@@ -7,8 +7,8 @@ public class App1
 		String ss="jdbc:mysql://localhost:3306/mydb";
 		try(Connection con=DriverManager.getConnection(ss,"root","root"))
 		{
-System.out.println("Implementation class is\t"+con.get Class()); 
-Statement st=co n.createStatement();
+System.out.println("Implementation class is\t"+con.getClass()); 
+Statement st=con.createStatement();
 ResultSet rs=st.executeQuery("select * from dept");
 while(rs.next())
 {
