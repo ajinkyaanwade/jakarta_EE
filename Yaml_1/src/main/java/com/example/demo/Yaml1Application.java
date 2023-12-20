8ab9b87524ba35015f550433d5e560dfcab44932
ajinkyaanwade <147714454+ajinkyaanwade@users.noreply.github.com>
2023-12-20 15:29:31 +0530

    Add files via upload
    YAML

---
+++ Yaml_1/src/main/java/com/example/demo/Yaml1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yaml1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yaml1Application.class, args);
+	}
+
+}
