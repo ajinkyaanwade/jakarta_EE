74844c0ca2752ab8f21e4b0633afa0b02a83e45c
ajinkyaanwade <147714454+ajinkyaanwade@users.noreply.github.com>
2023-12-21 19:49:24 +0530

    Add files via upload
    Second_rest

---
+++ Second_Rest/src/main/java/com/example/demo/SecondRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondRestApplication.class, args);
+	}
+
+}
